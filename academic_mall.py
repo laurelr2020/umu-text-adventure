be8f06ecb64008c7199d2da8586af9f097056433
laurelr2020 <reganlaurell@gmail.com>
2019-11-12 21:37:52 -0500

    refactor files

---
+++ academic_mall.py
@@ -0,0 +1,6 @@
+def enter_academic_mall():
+    print("This is the Academic Mall. " +
+        "To the left, you will see EBB. " + 
+        "To the right, you will see the old part of KHIC " +
+        "and straight ahead you see the new part of KHIC.")
+    
