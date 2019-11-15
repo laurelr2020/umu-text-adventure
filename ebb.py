aa3cd5f8d343c772bb3da5bed9b798add83323e0
laurelr2020 <reganlaurell@gmail.com>
2019-11-15 10:18:28 -0500

    add first choice when entering ebb

--- ebb.py
+++ ebb.py
@@ -1,2 +1,11 @@
 def enter_ebb_south():
-    print("Welcome to EBB")
+    print("Welcome to EBB! \nChoose where you want to go:" )
+    choice = input("Business Department or Upstairs")
+
+
+def enter_bus_dept():
+    print("Welcome to the Business Department!")
+
+def ebb_second_floor():
+    print("You're on the second floor")
+
