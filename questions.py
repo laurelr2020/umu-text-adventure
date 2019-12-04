4d5c6e0f874222c8f10cf9b8c7ac3e2b240e88f2
laurelr2020 <reganlaurell@gmail.com>
2019-12-04 18:02:16 -0500

    replace double quote with single quote

--- questions.py
+++ questions.py
@@ -6,71 +6,71 @@ def business_questions():
         'management': '',
         'marketing': ''
     }
-    print("Finance: What is a stock?\n" + 
-        "    A) a type of investment that represents an ownership share in a company \n" +
-        "    B) a garment for the foot and lower part of the leg \n" +
-        "    C) life-threatening condition that occurs when the body is not getting enough blood flow \n" +
-        "    D) a small amount of food eaten between meals\n"
+    print('Finance: What is a stock?\n' + 
+        '    A) a type of investment that represents an ownership share in a company \n' +
+        '    B) a garment for the foot and lower part of the leg \n' +
+        '    C) life-threatening condition that occurs when the body is not getting enough blood flow \n' +
+        '    D) a small amount of food eaten between meals\n'
     )
 
-    fin_correct_answer = "a"
-    finance_answer = input("Your answer > ")
-    finance_answer.replace(")", "")
+    fin_correct_answer = 'a'
+    finance_answer = input('Your answer > ')
+    finance_answer.replace(')', '')
 
     if(finance_answer.lower() == fin_correct_answer):
         business_degree['finance'] = 'A+'
     else:
         business_degree['finance'] = 'F'
-    print("Grade: " + business_degree['finance'])
+    print('Grade: ' + business_degree['finance'])
     
-    print("Mangement: What is mangagement?\n" +
-        "    A) the process of designing, launching and running a new business, which is often initially a small business.\n" +
-        "    B) the business process of creating relationships with and satisfying customers. \n" +
-        "    C) a set of principles relating to the functions of planning, organizing, directing and controlling,\n" +
-        "      and the application of these principles in harnessing physical, financial, human and informational \n" +
-        "      resources efficiently and effectively to achieve organizational goals.\n" + 
-        "    D) the social science that studies the production, distribution, and consumption of goods and services\n"
+    print('Mangement: What is mangagement?\n' +
+        '    A) the process of designing, launching and running a new business, which is often initially a small business.\n' +
+        '    B) the business process of creating relationships with and satisfying customers. \n' +
+        '    C) a set of principles relating to the functions of planning, organizing, directing and controlling,\n' +
+        '      and the application of these principles in harnessing physical, financial, human and informational \n' +
+        '      resources efficiently and effectively to achieve organizational goals.\n' + 
+        '    D) the social science that studies the production, distribution, and consumption of goods and services\n'
     )
-    mgt_correct_answer = "c"
-    management_answer = input("Your answer > ")
-    management_answer.replace(")", "")
+    mgt_correct_answer = 'c'
+    management_answer = input('Your answer > ')
+    management_answer.replace(')', '')
 
     if(management_answer.lower() == mgt_correct_answer):
         business_degree['management'] = 'A+'
     else:
         business_degree['management'] = 'F'
-    print("Grade: " + business_degree['management'])
+    print('Grade: ' + business_degree['management'])
 
-    print("Marketing: Which of the following is not inlcuded in marketing?\n" +
-        "    A) Advertising products\n" +
-        "    B) Selling products\n" +
-        "    C) Delivering products\n"
-        "    D) Firing employees"
+    print('Marketing: Which of the following is not inlcuded in marketing?\n' +
+        '    A) Advertising products\n' +
+        '    B) Selling products\n' +
+        '    C) Delivering products\n'
+        '    D) Firing employees'
     )
-    mkt_correct_answer = "d"
-    marketing_answer = input("Your answer > ")
-    marketing_answer.replace(")", "")
+    mkt_correct_answer = 'd'
+    marketing_answer = input('Your answer > ')
+    marketing_answer.replace(')', '')
 
     if(marketing_answer.lower() == mkt_correct_answer):
         business_degree['marketing'] = 'A+'
     else:
         business_degree['marketing'] = 'F'
-    print("Grade: " + business_degree['marketing'])
+    print('Grade: ' + business_degree['marketing'])
     
     player.business_grades = business_degree
 
 # @staticmethod
 # def graduation():
-#     print("Let's look at how you did.")
+#     print('Let's look at how you did.')
 #     for course, grade in business_degree.items():
-#         print(f"course: {course} grade: {grade}\n")
+#         print(f'course: {course} grade: {grade}\n')
 
 #     earned_degree = all(value == 'A+' for value in business_degree.values())
 
 #     if(earned_degree):
-#         print("CONGRATULATIONS!!! You have earned business degree!!!!")
+#         print('CONGRATULATIONS!!! You have earned business degree!!!!')
 #     else:
-#         print("I am sorry, but we are unable to award you a degree in business. Please feel free to take the courses again.")
+#         print('I am sorry, but we are unable to award you a degree in business. Please feel free to take the courses again.')
 
 
 def engineering_questions():
@@ -87,14 +87,14 @@ def engineering_questions():
     )
 
     civil_correct_answer = 'b'
-    civil_answer = input("Your answer > ").replace(")", "")
+    civil_answer = input('Your answer > ').replace(')', '')
 
     if(civil_answer.lower() == civil_correct_answer):
         engineering_grades['civil'] = 'A+'
     else:
         engineering_grades['civil'] = 'F'
 
-    print("Grade: " + engineering_grades['civil'])
+    print('Grade: ' + engineering_grades['civil'])
 
     print('Computer Engineering: what is computer engineering?\n' +
         '    A)  the knowledge and ability to utilize computers and related technology efficiently\n' +
@@ -103,14 +103,14 @@ def engineering_questions():
         '    D)  a branch of digital forensic science pertaining to evidence found in computers and digital storage media\n'
     )
     computer_correct_answer = 'c'
-    computer_answer = input("Your answer > ").replace(")", "")
+    computer_answer = input('Your answer > ').replace(')', '')
 
     if(computer_answer.lower() == computer_correct_answer):
         engineering_grades['computer'] = 'A+'
     else:
         engineering_grades['computer'] = 'F'
 
-    print("Grade: " + engineering_grades['computer'])
+    print('Grade: ' + engineering_grades['computer'])
 
     print('Mechanical Engineering: what is mechanical engineering?\n' +
         '    A)  a measure of the force amplification achieved by using a tool,\n' +
@@ -121,14 +121,14 @@ def engineering_questions():
     )
 
     mechanical_correct_answer = 'c'
-    mechanical_answer = input("Your answer > ").replace(")", "")
+    mechanical_answer = input('Your answer > ').replace(')', '')
 
     if(mechanical_answer.lower() == mechanical_correct_answer):
         engineering_grades['mechanical'] = 'A+'
     else:
         engineering_grades['mechanical'] = 'F'
 
-    print("Grade: " + engineering_grades['mechanical'])
+    print('Grade: ' + engineering_grades['mechanical'])
 
     player.engineering_grades = engineering_grades
 
@@ -147,4 +147,4 @@ def psychology_questions():
     )
     
     cognitive_correct_answer = 'A'
-    cognitive_answer = input("Your answer > ").replace(")", "")
+    cognitive_answer = input('Your answer > ').replace(')', '')
