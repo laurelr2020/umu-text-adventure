c9cfe6b627ea964e30123665a8389ac6dd1ba9e4
laurelr2020 <reganlaurell@gmail.com>
2019-12-08 21:18:27 -0500

    add foreign language dept

--- khic.py
+++ khic.py
@@ -1,3 +1,4 @@
+from questions import foreign_language_questions
 def enter_khic_north():
     print('Welcome to KHIC! Would you like to go to the Foreign Language Department or the Circulation Desk?')
     choice = input('Language or Circulation').lower()
@@ -9,11 +10,26 @@ def enter_khic_north():
     else:
         enter_khic_north()
 
+def exit_khic_north():
+    print('You have exited KHIC North')
+
 def enter_khic_east():
     print('Welcome to KHIC')
 
 def enter_foreign_language_dept():
     print('Welcome to the Foreign Language Department')
+    foreign_language_questions()
+    exit_foreign_language_dept()
+
+def exit_foreign_language_dept():
+    print('You have exit the Foreign Language Department. Where would you like to go? Circulation Desk or Exit KHIC')
+    choice = input('Circulation or Exit').lower()
+
+    if('circ' in choice):
+        enter_circulation_desk()
+    elif('exit' in choice):
+        exit_khic_north()
+
 
 def enter_circulation_desk():
     print('Welcome to the Circulation Desk')
